16b7ca860344f8392a10c52fe2459b6596f97156
haristotle <haristotlepatil2003@gmail.com>
2025-08-28 02:04:07 +0530

    Implement Form component A form component is a generic form component, specific forms are all components that return a Form component by passing required props

---
+++ src/components/Form.jsx
@@ -0,0 +1,177 @@
+import dropdownDownButton from "../assets/icons/arrow-down-drop-circle-outline.svg";
+import dropdownUpButton from "../assets/icons/arrow-up-drop-circle-outline.svg";
+
+function InputGroup({ type, id, label, value, pattern }) {
+  const TEXTAREA_ROWSIZE = 4;
+  const TEXTAREA_COLSIZE = 50;
+
+  return (
+    <div className="input-group">
+      {type === "textarea" && (
+        <>
+          <textarea
+            id={id}
+            rows={TEXTAREA_ROWSIZE}
+            cols={TEXTAREA_COLSIZE}
+            placeholder=" "
+            required
+          ></textarea>
+          <label htmlFor={id}>{label}</label>
+        </>
+      )}
+      {type === "submit" && (
+        <>
+          <input type={type} value={value} placeholder=" " />
+        </>
+      )}
+      {type !== "submit" && type !== "textarea" && (
+        <>
+          <input
+            type={type}
+            id={id}
+            pattern={pattern}
+            placeholder=" "
+            required
+          />
+          <label htmlFor={id}>{label}</label>
+        </>
+      )}
+    </div>
+  );
+}
+
+function Form({
+  id,
+  formTitle,
+  dropdownButtonClickHandler,
+  formSubmissionHandler,
+  formVisible,
+  inputGroupArray,
+}) {
+  return (
+    <form id={`${id}-form`} onSubmit={formSubmissionHandler}>
+      <div className="title-dropdown-button">
+        <h1>{formTitle}</h1>
+        <button onClick={dropdownButtonClickHandler} className="dropdown-btn">
+          <img
+            src={formVisible ? dropdownUpButton : dropdownDownButton}
+            alt="dropdown-icon"
+            className="icon"
+          />
+        </button>
+      </div>
+
+      <div className={`all-inputs-container ${formVisible || "hidden"}`}>
+        {inputGroupArray.map((inputGroup) => (
+          <InputGroup key={inputGroup.id} {...inputGroup} />
+        ))}
+      </div>
+    </form>
+  );
+}
+
+export function GeneralInformationForm({
+  dropdownButtonClickHandler,
+  formSubmissionHandler,
+}) {
+  const inputGroupArray = [
+    { type: "text", id: "name", label: "Name" },
+    { type: "tel", id: "phone-number", label: "Phone" },
+    { type: "email", id: "email", label: "Email" },
+    { type: "url", id: "website", label: "Website" },
+    { type: "textarea", id: "about", label: "Introduce yourself" },
+    { type: "submit", value: "Submit" },
+  ];
+
+  return (
+    <Form
+      id={"general-information"}
+      formTitle={"General Information"}
+      dropdownButtonClickHandler={dropdownButtonClickHandler}
+      formSubmissionHandler={formSubmissionHandler}
+      formVisible={true}
+      inputGroupArray={inputGroupArray}
+    />
+  );
+}
+
+export function ExperienceInformationForm({
+  dropdownButtonClickHandler,
+  formSubmissionHandler,
+}) {
+  const inputGroupArray = [
+    { type: "text", id: "company-name", label: "Company Name" },
+    {
+      type: "text",
+      id: "experience-start-year",
+      pattern: "[12][0-9]{3}",
+      label: "Start year",
+    },
+    {
+      type: "text",
+      id: "experience-end-year",
+      pattern: "[12][0-9]{3}",
+      label: "End year",
+    },
+    { type: "text", id: "job-title", label: "Job title" },
+    { type: "textarea", id: "role-description", label: "Describe your role" },
+    { type: "submit", value: "Add" },
+  ];
+
+  return (
+    <Form
+      id={"experience"}
+      formTitle={"Experience"}
+      dropdownButtonClickHandler={dropdownButtonClickHandler}
+      formSubmissionHandler={formSubmissionHandler}
+      formVisible={true}
+      inputGroupArray={inputGroupArray}
+    />
+  );
+}
+
+export function SkillsForm({
+  dropdownButtonClickHandler,
+  formSubmissionHandler,
+}) {
+  const inputGroupArray = [
+    { type: "text", id: "skill", label: "Skill (Java, C++ etc.)" },
+    { type: "submit", value: "Add" },
+  ];
+
+  return (
+    <Form
+      id={"skills"}
+      formTitle={"Skills"}
+      dropdownButtonClickHandler={dropdownButtonClickHandler}
+      formSubmissionHandler={formSubmissionHandler}
+      formVisible={true}
+      inputGroupArray={inputGroupArray}
+    />
+  );
+}
+
+export function LanguageForm({
+  dropdownButtonClickHandler,
+  formSubmissionHandler,
+}) {
+  const inputGroupArray = [
+    {
+      type: "text",
+      id: "language",
+      label: "Language (English, Japanese etc.)",
+    },
+    { type: "submit", value: "Add" },
+  ];
+
+  return (
+    <Form
+      id={"language"}
+      formTitle={"Language"}
+      dropdownButtonClickHandler={dropdownButtonClickHandler}
+      formSubmissionHandler={formSubmissionHandler}
+      formVisible={true}
+      inputGroupArray={inputGroupArray}
+    />
+  );
+}
