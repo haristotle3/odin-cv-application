f4cd9351ba8422ded8119edc8419316b02fc0f9d
haristotle <haristotlepatil2003@gmail.com>
2025-08-29 19:27:25 +0530

    Pass formEditHandlers as props to CVContainer

--- src/components/CVContainer.jsx
+++ src/components/CVContainer.jsx
@@ -5,21 +5,25 @@ import "../styles/cvContainer.css";
 export default function CVContainer({
   generalInformationSection,
   experienceSection,
+  experienceFormEditHandler,
   educationSection,
+  educationFormEditHandler,
   skillsSection,
+  skillsFormEditHandler,
   languagesSection,
+  languagesFormEditHandler
 }) {
   return (
     <div className="cv-container">
       <GeneralInformation {...generalInformationSection} />
       <div className="non-general-information">
         <div className="experience-education-container">
-          <Section {...experienceSection} />
-          <Section {...educationSection} />
+          <Section {...experienceSection} editClickHandler={experienceFormEditHandler} />
+          <Section {...educationSection} editClickHandler={educationFormEditHandler}/>
         </div>
         <div className="language-skills-container">
-          <Section {...skillsSection} />
-          <Section {...languagesSection} />
+          <Section {...skillsSection} editClickHandler={skillsFormEditHandler}/>
+          <Section {...languagesSection} editClickHandler={languagesFormEditHandler}/>
         </div>
       </div>
     </div>
